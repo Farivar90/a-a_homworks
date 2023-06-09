0dc2414f4f0b23617dcd8924f12ce09be9a89306
farivar <farivaramiri@gmail.com>
2023-06-08 22:39:44 -0700

    map

---
+++ w3d5/practice-for-ch-ruby-abstract-data-types-main/practice-for-ch-ruby-abstract-data-types-main/map.rb
@@ -0,0 +1,36 @@
+
+class Map
+    def initialize
+      @map = []
+    end
+  
+    def set(key, value)
+      pair_index = find_index(key)
+      if pair_index
+        @map[pair_index][1] = value
+      else
+        @map.push([key, value])
+      end
+    end
+  
+    def get(key)
+      pair = @map.find { |k, _| k == key }
+      pair.nil? ? nil : pair[1]
+    end
+  
+    def delete(key)
+      pair_index = find_index(key)
+      @map.delete_at(pair_index) if pair_index
+    end
+  
+    def show
+      @map.each { |pair| puts "#{pair[0]} => #{pair[1]}" }
+    end
+  
+    private
+  
+    def find_index(key)
+      @map.each_with_index { |pair, index| return index if pair[0] == key }
+      nil
+    end
+end
